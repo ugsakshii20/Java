c3198efac1026768ea67176fda1065eb4bd04252
ugsakshii20 <138098129+ugsakshii20@users.noreply.github.com>
2024-01-24 16:15:36 +0530

    add.java

---
+++ add.java
@@ -0,0 +1,8 @@
+public class add {
+    public static void main(String[] args) {
+        int i=10;
+        int j=20;
+        int sum=i+j;
+        System.out.println("Addition is="+sum);
+    }
+}
